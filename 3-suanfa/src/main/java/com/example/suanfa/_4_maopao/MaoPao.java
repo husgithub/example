26f12965d2e87e6064a25e4e52036a5a70735fcb
husong <husong_163mailbox@163.com>
2019-06-16 16:30:18 +0800

    算法

--- 3-suanfa/src/main/java/com/example/suanfa/_4_maopao/MaoPao.java
+++ 3-suanfa/src/main/java/com/example/suanfa/_4_maopao/MaoPao.java
@@ -15,7 +15,7 @@ public class MaoPao {
     public void sortFromSmallToLarge(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j] < array[j + 1]) {
+                if (array[j] > array[j + 1]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
@@ -32,7 +32,7 @@ public class MaoPao {
     public void sortFromLargeToSmall(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j] > array[j + 1]) {
+                if (array[j] < array[j + 1]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
